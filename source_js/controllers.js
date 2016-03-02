b28c684f3a30cc79297d2a94e45135c6096162f7
Sujay Khandekar <khandek2@illinois.edu>
2016-03-01 22:07:54 -0600

    starter page

--- source_js/controllers.js
+++ source_js/controllers.js
@@ -1,8 +1,1 @@
 /* Sample Controller */
-
-// demoApp.controller('demoController', ['$scope', '$http', function($scope, $http) {
-//   $http.get('public/data/cs498rk.json').success(function(data) {
-//     $scope.courseInfo = data;
-//     $scope.artistOrder = 'name';
-//   });
-// }]);
